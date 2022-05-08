1143b9efa1e36f959674efed0816d2dee37b3585
rka <russkikh_k@ekb.sima-land.ru>
2022-05-08 21:11:04 +0500

    +

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,10 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 
 const url = 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/'
 
 export default function Details({id}) {
     const [obj, setObj] = useState(undefined)
+    const load = useRef(false)
 
     useEffect(() => {
         if (id === '0') {
@@ -11,11 +12,31 @@ export default function Details({id}) {
         }
         fetch(url + id + '.json')
             .then(response => response.json())
-            .then(data => setObj(data))
+            .then(data => {
+                load.current = false
+                setObj(data)
+            })
+        load.current = true
+        setObj(undefined)
     }, [id])
 
+    if (obj === undefined && !load.current) {
+        return <></>
+    }
+
+    if (load.current) {
+        return (
+            <div className={'details'}>
+                <div className="preloader">
+                    <div className="preloader__row">
+                        <div className="preloader__item"></div>
+                        <div className="preloader__item"></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
-        obj &&
         <div className={'details'}>
             <img alt={obj.name} className={'detail-image'} src={obj.avatar}/>
             <div className={'detail details-name'}>
